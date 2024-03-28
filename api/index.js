035af766e26fee77d3442141b773fd065ce2df47
tarupathak <pathaktaru2002@gmail.com>
2024-03-28 20:40:18 +0530

    Added register api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.post('/register', (req,res)=>{
-    res.json('test ok2');
-})
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
+});
 
 app.listen(3001);
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(event) {
+    event.preventDefault();
+    await fetch("http://localhost:3001/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
   return (
     <div className="mt-6 flex items-center justify-center sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -10,7 +22,7 @@ const Register = () => {
             Register new account
           </h2>
         </div>
-        <form className="mt-8 space-y-6">
+        <form className="mt-8 space-y-6" onSubmit={register}>
           <div className="rounded-md shadow-sm -space-y-px ">
             <div className="mb-8">
               <label htmlFor="email-address" className="sr-only">
@@ -22,6 +34,8 @@ const Register = () => {
                 type="email"
                 autoComplete="email"
                 required
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
                 className="appearance-none rounded relative block w-full px-3 py-4 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
               />
@@ -36,6 +50,8 @@ const Register = () => {
                 type="password"
                 autoComplete="current-password"
                 required
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
                 className="appearance-none rounded relative block w-full px-3 py-4 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
@@ -51,7 +67,7 @@ const Register = () => {
             </button>
           </div>
           <div className="text-center">
-            Already have an account?{" "}   
+            Already have an account?{" "}
             <span className="text-[blue]">
               <Link to="/login">Login</Link>
             </span>
