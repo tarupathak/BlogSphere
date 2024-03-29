4e813e3adbc1a9c6f4285dff611b11e1ee909fe9
tarupathak <pathaktaru2002@gmail.com>
2024-03-29 12:39:22 +0530

    Added bcrypt js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose"); // Import mongoose
 const User = require("./models/User");
+const bcrypt = require('bcryptjs');
 const app = express();
 
 app.use(cors());
@@ -12,22 +13,25 @@ mongoose.connect(
   
 );
 
+const salt = bcrypt.genSaltSync(10);
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  try{
     const userDoc = await User.create({
       username,
-      password
+      password:bcrypt.hashSync(password,salt),
     });
     res.json(userDoc);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
   }
 });
 
 app.listen(8080, () => {
-  console.log("Server is running on port 3001");
+  console.log("Server is running on port 8080");
 });
