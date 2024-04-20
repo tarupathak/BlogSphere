a1a05ce7153ae63a25029fe8a1b289dbea6fd0aa
tarupathak <pathaktaru2002@gmail.com>
2024-04-20 18:45:58 +0530

    Added logout functionality

--- api/index.js
+++ api/index.js
@@ -2,57 +2,69 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose"); // Import mongoose
 const User = require("./models/User");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const app = express();
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use;
 
 mongoose.connect(
-  "mongodb+srv://pathaktaru2002:12345@cluster0.yb85zcz.mongodb.net/BlogSphere",
-  
+  "mongodb+srv://pathaktaru2002:12345@cluster0.yb85zcz.mongodb.net/BlogSphere"
 );
 
 const salt = bcrypt.genSaltSync(10);
-const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-
+const secret = "asdfe45we45w345wegw345werjktjwertkj";
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.post('/register', async (req,res) => {
-  const {username,password} = req.body;
-  try{
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
     const users = await User.create({
       username,
-      password:bcrypt.hashSync(password,salt),
+      password: bcrypt.hashSync(password, salt),
     });
     res.json(users);
-  } catch(e) {
+  } catch (e) {
     console.log(e);
     res.status(400).json(e);
   }
 });
 
-app.post('/login', async (req,res) => {
-  const {username,password} = req.body;
-  const users = await User.findOne({username});
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const users = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, users.password);
   if (passOk) {
-    jwt.sign({username,id:users._id}, secret, {}, (err,token) => {
+    jwt.sign({ username, id: users._id }, secret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie('token', token).json({
-        id:users._id,
+      res.cookie("token", token).json({
+        id: users._id,
         username,
       });
     });
   } else {
-    res.status(400).json('wrong credentials');
+    res.status(400).json("wrong credentials");
   }
 });
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+  res.json(req.cookies);
+});
+
+app.post('/logout',(req,res)=>{
+  res.cookie('token', '').json('ok');
+})
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUsername(userInfo.username);
+      });
+    });
+  }, []);
+
+  function logout(){
+    fetch('http://localhost:8080/logout',{
+      credentials: 'include',
+      method: 'POST'
+    })
+    setUsername(null);
+  }
+
   return (
     <div className="flex items-center justify-between">
       <div className="text-bold text-black pacifico-regular">
         <Link to="/">BlogSphere</Link>
       </div>
       <div className="flex items-center justify-around gap-12 text-lg">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <Link onClick={logout}>Logout</Link>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </div>
     </div>
   );
