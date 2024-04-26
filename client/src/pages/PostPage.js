f14aec0f5f6f22e40e131d84a04a583e33028a29
tarupathak <pathaktaru2002@gmail.com>
2024-04-26 20:07:40 +0530

    Revert "Added image in tah postpage section"
    This reverts commit 5f21b821ab1d88ce6aae4fbf865d75c687ee20c3.

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,30 +1,9 @@
-import {useContext, useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {formatISO9075} from "date-fns";
-import {UserContext} from "../UserContext";
-import {Link} from 'react-router-dom';
+import React from 'react'
 
 const PostPage = () => {
-    const [postInfo,setPostInfo] = useState(null);
-    const {userInfo} = useContext(UserContext);
-    const {id} = useParams();
-    useEffect(() => {
-      fetch(`http://localhost:8080/post/${id}`)
-        .then(response => {
-          response.json().then(postInfo => {
-            setPostInfo(postInfo);
-          });
-        });
-    }, []);
-  
-    if (!postInfo) return '';
-
-  if (!postInfo) return "";
   return (
-    <div>
-      <img src={`http://localhost:8080/${postInfo.cover}`} alt="postimg" />
-    </div>
-  );
-};
+    <div>PostPage</div>
+  )
+}
 
-export default PostPage;
+export default PostPage
