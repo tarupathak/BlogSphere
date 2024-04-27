54a2f0b863f0e6eab726a5855d1a37f637fbcbfa
tarupathak <pathaktaru2002@gmail.com>
2024-04-27 15:24:47 +0530

    Modified the styling of post page

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,23 +4,25 @@ import { formatISO9075 } from "date-fns";
 
 const Post = ({ _id, title, summary, cover, content, createdAt, author }) => {
   return (
-    <div className="grid grid-cols-6 items-start gap-6 mt-8 border border-gray-300 rounded-lg p-4 mb-12">
-  <div className="h-auto col-span-1 w-40">
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 items-start gap-6 mt-8 border border-gray-300 rounded-lg p-4 mb-12">
+  <div class="h-auto lg:col-span-1 lg:w-40">
     <Link to={`/post/${_id}`}>
       <img src={"http://localhost:8080/" + cover} alt="image" />
     </Link>
   </div>
-  <div className="grid col-span-5  gap-4">
+  <div class="grid sm:col-span-2 lg:col-span-5 gap-4">
     <Link to={`/post/${_id}`}>
-      <div className="text-2xl font-bold">{title}</div>
+      <div class="text-lg lg:text-2xl font-bold">{title}</div>
     </Link>
-    <p className="text-xs font-bold text-gray">
+    <p class="text-xs font-bold text-gray">
       {/* <a className="">{author}</a> */}
       <time>{formatISO9075(new Date(createdAt))}</time>
     </p>
     <p>{summary}</p>
   </div>
 </div>
+
+
 
 
   );
